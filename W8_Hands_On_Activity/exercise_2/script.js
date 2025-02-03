d7d8d889cd0735c13936734088337cc2baac58aa
HSatsss <hannasato14@gmail.com>
2025-02-03 22:31:39 +0800

    commit

--- W8_Hands_On_Activity/exercise_2/script.js
+++ W8_Hands_On_Activity/exercise_2/script.js
@@ -1,9 +1,9 @@
-// JSON string
-const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
-// Parse JSON string into a JavaScript object
-const jsonObject = JSON.parse(jsonString);
-console.log(jsonObject.name); // Output: John
-console.log(jsonObject.age); // Output: 30
-// Convert JavaScript object back to JSON string
-const newJsonString = JSON.stringify(jsonObject);
-console.log(newJsonString); // Output: {"name":"John","age":30,"city":"New York"}
+const person = {
+    name: "Hanna Keziah T.Sato",
+    age: 20,
+    hobbies: ["music", "coding", "gaming"]
+};
+
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
+console.log(newJsonString);
